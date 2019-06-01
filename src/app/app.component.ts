b1b57f433c197c85e9f47d2d8d6391cd79759f03
Manishthakur1297 <Manishthakur1297@gmail.com>
2019-06-02 01:39:25 +0530

    First

---
+++ src/app/app.component.ts
@@ -0,0 +1,72 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'CustomTable';
+
+  headers = ["ID", "Name", "Age", "Gender", "Country"];
+
+  rows = [
+    {
+      "ID" : "1",
+      "Name" : "Rahul",
+      "Age" : "21",
+      "Gender" : "Male",
+      "Country" : "India"
+    },
+    {
+      "ID" : "2",
+      "Name" : "Ajay",
+      "Age" : "25",
+      "Gender" : "Male",
+      "Country" : "India"
+    },
+    {
+      "ID" : "3",
+      "Name" : "Vikram",
+      "Age" : "31",
+      "Gender" : "Male",
+      "Country" : "Australia"
+    },
+    {
+      "ID" : "4",
+      "Name" : "Riya",
+      "Age" : "20",
+      "Gender" : "Female",
+      "Country" : "India"
+    },
+    {
+      "ID" : "5",
+      "Name" : "John",
+      "Age" : "23",
+      "Gender" : "Male",
+      "Country" : "USA"
+    },
+    {
+      "ID" : "6",
+      "Name" : "Raman",
+      "Age" : "27",
+      "Gender" : "Male",
+      "Country" : "India"
+    },
+    {
+      "ID" : "7",
+      "Name" : "Mohan",
+      "Age" : "39",
+      "Gender" : "Male",
+      "Country" : "India"
+    },
+    {
+      "ID" : "8",
+      "Name" : "Shreya",
+      "Age" : "21",
+      "Gender" : "Female",
+      "Country" : "India"
+    }
+  ]
+
+}
